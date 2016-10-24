48f06cd7d71f797bcbde06ac6fae1a44a0451453
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2016-10-24 15:01:05 -0400

    add mapInverse methods

--- src/main/java/org/janelia/saalfeldlab/deform/RenderTransformMeshMappingWithMasks.java
+++ src/main/java/org/janelia/saalfeldlab/deform/RenderTransformMeshMappingWithMasks.java
@@ -49,13 +49,13 @@ public class RenderTransformMeshMappingWithMasks<T extends Type<T>> {
 		this.mesh = mesh;
 	}
 
-	final private class MapTriangleThread extends Thread {
-		final private AtomicInteger i;
-		final private List<Pair<AffineModel2D, double[][]>> triangles;
-		final RealRandomAccessible<T> source;
-		final RandomAccessibleInterval<T> target;
+	abstract private class AbstractMapTriangleThread extends Thread {
+		final protected AtomicInteger i;
+		final protected List<Pair<AffineModel2D, double[][]>> triangles;
+		final protected RealRandomAccessible<T> source;
+		final protected RandomAccessibleInterval<T> target;
 
-		MapTriangleThread(
+		AbstractMapTriangleThread(
 				final AtomicInteger i,
 				final List<Pair<AffineModel2D, double[][]>> triangles,
 				final RealRandomAccessible<T> source,
@@ -66,12 +66,45 @@ public class RenderTransformMeshMappingWithMasks<T extends Type<T>> {
 			this.source = source;
 			this.target = target;
 		}
+	}
+
+	final private class MapTriangleThread extends AbstractMapTriangleThread {
+
+		MapTriangleThread(
+				final AtomicInteger i,
+				final List<Pair<AffineModel2D, double[][]>> triangles,
+				final RealRandomAccessible<T> source,
+				final RandomAccessibleInterval<T> target) {
+
+			super(i, triangles, source, target);
+		}
 
 		@Override
 		final public void run() {
 			int k = i.getAndIncrement();
 			while (!isInterrupted() && k < triangles.size()) {
 				mapTriangle(triangles.get(k), source, target);
+				k = i.getAndIncrement();
+			}
+		}
+	}
+
+	final private class MapInverseTriangleThread extends AbstractMapTriangleThread {
+
+		MapInverseTriangleThread(
+				final AtomicInteger i,
+				final List<Pair<AffineModel2D, double[][]>> triangles,
+				final RealRandomAccessible<T> source,
+				final RandomAccessibleInterval<T> target) {
+
+			super(i, triangles, source, target);
+		}
+
+		@Override
+		final public void run() {
+			int k = i.getAndIncrement();
+			while (!isInterrupted() && k < triangles.size()) {
+				mapInverseTriangle(triangles.get(k), source, target);
 				k = i.getAndIncrement();
 			}
 		}
@@ -130,6 +163,74 @@ public class RenderTransformMeshMappingWithMasks<T extends Type<T>> {
 			final ArrayList<Thread> threads = new ArrayList<Thread>(numThreads);
 			for (int k = 0; k < numThreads; ++k) {
 				final Thread mtt = new MapTriangleThread(i, av, source, target);
+				threads.add(mtt);
+				mtt.start();
+			}
+			for (final Thread mtt : threads) {
+				try {
+					mtt.join();
+				} catch (final InterruptedException e) {}
+			}
+		} else {
+			for (final Pair<AffineModel2D, double[][]> triangle : av) {
+				mapTriangle(triangle, source, target);
+			}
+		}
+	}
+
+	final static protected <T extends Type<T>> void mapInverseTriangle(
+			final Pair<AffineModel2D, double[][]> ai,
+			final RealRandomAccessible<T> source,
+			final RandomAccessibleInterval<T> target) {
+
+		final long w = target.max(0);
+		final long h = target.max(1);
+
+		final AffineModel2D a = ai.getA();
+		final AffineTransform2D affine = new AffineTransform2D();
+		final double[] coefficients = new double[6];
+		a.toArray(coefficients);
+		affine.set(
+				coefficients[0], coefficients[2], coefficients[4],
+				coefficients[1], coefficients[3], coefficients[5]);
+		final double[][] pq = ai.getB();
+
+		final double[] min = new double[2];
+		final double[] max = new double[2];
+		RenderTransformMesh.calculateSourceBoundingBox(pq, min, max);
+
+		final long minX = Math.max(0, Util.roundPos(min[0]));
+		final long minY = Math.max(0, Util.roundPos(min[1]));
+		final long maxX = Math.min(w, Util.roundPos(max[0]));
+		final long maxY = Math.min(h, Util.roundPos(max[1]));
+
+		final RealRandomAccess<T> sourceAccess = source.realRandomAccess();
+		final RandomAccess<T> targetAccess = target.randomAccess(new FinalInterval(new long[] { minX, minY }, new long[] { maxX, maxY }));
+		for (long y = minY; y <= maxY; ++y) {
+			targetAccess.setPosition(y, 1);
+			for (long x = minX; x <= maxX; ++x) {
+				targetAccess.setPosition(x, 0);
+				if (RenderTransformMesh.isInSourceTriangle(pq, x, y)) {
+					try {
+						affine.apply(targetAccess, sourceAccess);
+					} catch (final Exception e) {
+						e.printStackTrace(System.err);
+						continue;
+					}
+
+					targetAccess.get().set(sourceAccess.get());
+				}
+			}
+		}
+	}
+
+	final public void mapInverse(final RealRandomAccessible<T> source, final RandomAccessibleInterval<T> target, final int numThreads) {
+		final ArrayList<Pair<AffineModel2D, double[][]>> av = mesh.getAV();
+		if (numThreads > 1) {
+			final AtomicInteger i = new AtomicInteger(0);
+			final ArrayList<Thread> threads = new ArrayList<Thread>(numThreads);
+			for (int k = 0; k < numThreads; ++k) {
+				final Thread mtt = new MapInverseTriangleThread(i, av, source, target);
 				threads.add(mtt);
 				mtt.start();
 			}
