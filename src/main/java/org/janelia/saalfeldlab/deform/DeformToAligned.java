599f5d77a0ff3ffa3792049c80d1eddae01b2e58
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2017-11-02 15:59:43 -0400

    fix and finish DeformToAlign

--- src/main/java/org/janelia/saalfeldlab/deform/DeformToAligned.java
+++ src/main/java/org/janelia/saalfeldlab/deform/DeformToAligned.java
@@ -6,13 +6,9 @@ package org.janelia.saalfeldlab.deform;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
-
-import org.junit.experimental.theories.ParameterSignature;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
@@ -28,7 +24,6 @@ import bdv.util.LocalIdService;
 import ch.systemsx.cisd.hdf5.HDF5Factory;
 import ch.systemsx.cisd.hdf5.IHDF5Reader;
 import ch.systemsx.cisd.hdf5.IHDF5Writer;
-import io.scif.img.cell.SCIFIOCellImgCleaningProvider;
 import mpicbg.trakem2.transform.CoordinateTransform;
 import mpicbg.trakem2.transform.CoordinateTransformList;
 import net.imglib2.FinalInterval;
@@ -71,6 +66,9 @@ public class DeformToAligned {
 		@Parameter(names = { "--label", "-l" }, description = "label dataset, multiple entries possible" )
 		public List<String> labels = new ArrayList<>();
 
+		@Parameter(names = { "--label_annotation_offset" }, description = "optional labe and annotation offset if data comes from multiple files (padded raw, non-padded input)")
+		private String annotationOffsetString = null;
+
 		@Parameter(names = { "--outfile", "-o" }, required = true, description = "output CREMI-format HDF5 file name")
 		public String outFile;
 
@@ -79,9 +77,26 @@ public class DeformToAligned {
 
 		@Parameter(names = { "--meshcellsize", "-c" }, description = "mesh cell size for rendering the output")
 		public long meshCellSize = 64;
-		
+
 		@Parameter(names = { "--numProcessors" }, description = "number of processors for parallel mapping, default all")
 		public int numProcessors = Runtime.getRuntime().availableProcessors();
+
+		public static double[] getReorderedDoubleArray(final String csv) {
+			final String[] valueStrings = csv.split(",");
+			final double[] values = new double[ valueStrings.length ];
+			for (int i = 0; i < valueStrings.length; ++i)
+				values[values.length - i - 1] = Double.parseDouble(valueStrings[i]);
+
+			return values;
+		}
+
+		public double[] getAnnotationOffset() {
+
+			if (annotationOffsetString == null)
+				return null;
+			else
+				return getReorderedDoubleArray(annotationOffsetString);
+		}
 	}
 
 	final static private int[] cellDimensions = new int[] { 64, 64, 8 };
@@ -130,7 +145,7 @@ public class DeformToAligned {
 			mapping.map(sourceSlice, targetSlice, numProcessors);
 		}
 	}
-	
+
 	/**
 	 * Creates a transformed version of a 3D volume by mapping each slice with
 	 * a mesh transform, mpicbg style.
@@ -161,21 +176,21 @@ public class DeformToAligned {
 
 	/**
 	 * @param args
-	 * @throws Exception 
+	 * @throws Exception
 	 */
 	public static void main(final String... args) throws Exception {
 
 		final Parameters params = new Parameters();
 		new JCommander(params, args);
-		
+
 		System.out.printf("Opening inputs: \"%s\", \"%s\", \"%s\"...", params.inFile, params.inFileLabels, params.inFileAnnotations);
 		System.out.println();
-		
+
 		final IHDF5Reader rawReader = HDF5Factory.openForReading(params.inFile);
 		final IHDF5Reader labelsReader = params.inFileLabels == null ? rawReader : HDF5Factory.openForReading(params.inFileLabels);
 		if (params.inFileAnnotations == null)
 			params.inFileAnnotations = params.inFile;
-		
+
 		System.out.println("Opening " + params.outFile + " for writing");
 		final File outFile = new File(params.outFile);
 		final IHDF5Writer writer = HDF5Factory.open(params.outFile);
@@ -187,7 +202,7 @@ public class DeformToAligned {
 		}
 		if (trakem2Export == null)
 			throw new FileNotFoundException("Could not find transforms " + params.inTransformations);
-		
+
 		final ArrayList<CoordinateTransform> transforms = new ArrayList<>();
 		for (final ArrayList<TransformSpec> transformList : trakem2Export.transforms) {
 			final CoordinateTransformList<CoordinateTransform> ctl = new CoordinateTransformList<>();
@@ -202,20 +217,20 @@ public class DeformToAligned {
 						trakem2Export.width,
 						trakem2Export.height,
 						trakem2Export.transforms.size());
-		
+
 		double[] transformResolution = null;
-		
+
 		/* raw pixels */
 		for (final String rawPath : params.raws) {
-			
+
 			final RandomAccessibleInterval<UnsignedByteType> rawSource = Util.loadRaw(rawReader, rawPath, cellDimensions);
-			
-			double[] resolution = H5Utils.loadAttribute(rawReader, rawPath, "resolution");
+
+			final double[] resolution = H5Utils.loadAttribute(rawReader, rawPath, "resolution");
 			if (transformResolution == null)
-				transformResolution = resolution; 
-			
+				transformResolution = resolution;
+
 			H5Utils.createUnsignedByte(writer, rawPath, sourceInterval, cellDimensions);
-			
+
 			/* process in mellSize[2] thick slices to save some memory */
 			for (int zOffset = 0; zOffset < sourceInterval.dimension(2); zOffset += cellDimensions[2]) {
 
@@ -246,28 +261,45 @@ public class DeformToAligned {
 						rawPath,
 						cellDimensions);
 			}
-			
+
 			if (resolution != null)
 				H5Utils.saveAttribute(resolution, writer, rawPath, "resolution");
 			H5Utils.saveAttribute(new double[]{0, 0, 0}, writer, rawPath, "offset");
 		}
+
+		final double[] offset = params.getAnnotationOffset();
 
 		/* labels */
 		for (final String labelsPath : params.labels) {
 
 			final RandomAccessibleInterval<LabelMultisetType> labelsSource = Util.loadLabels(labelsReader, labelsPath, cellDimensions);
 
-			double[] resolution = H5Utils.loadAttribute(labelsReader, labelsPath, "resolution");
+			final double[] resolution = H5Utils.loadAttribute(labelsReader, labelsPath, "resolution");
 			if (transformResolution == null)
 				transformResolution = resolution;
-			
-			final RandomAccessibleInterval<LongType> longLabelsSource = Converters.convert(labelsSource,
-					new Converter<LabelMultisetType, LongType>() {
-						@Override
-						public void convert(final LabelMultisetType a, final LongType b) {
-							b.set(a.entrySet().iterator().next().getElement().id());
-						}
-					}, new LongType());
+
+			/* override label offset with parameter */
+			final RandomAccessibleInterval<LabelMultisetType> translatedLabelsSource;
+			if (offset == null)
+				translatedLabelsSource = labelsSource;
+			else
+				translatedLabelsSource = Views.translate(
+						Views.zeroMin(labelsSource),
+						new long[]{
+								Math.round(offset[0] / resolution[2]),
+								Math.round(offset[1] / resolution[1]),
+								Math.round(offset[2] / resolution[0])});
+
+			final RandomAccessibleInterval<LongType> longLabelsSource =
+					Converters.convert(
+							translatedLabelsSource,
+							new Converter<LabelMultisetType, LongType>() {
+								@Override
+								public void convert(final LabelMultisetType a, final LongType b) {
+									b.set(a.entrySet().iterator().next().getElement().id());
+								}
+							},
+							new LongType());
 
 			H5Utils.createUnsignedLong(writer, labelsPath, sourceInterval, cellDimensions);
 
@@ -278,7 +310,7 @@ public class DeformToAligned {
 				final RandomAccessibleInterval<LongType> labelsTarget =
 						Views.translate(
 								PlanarImgs.longs(
-										sourceInterval.dimension(0),
+										sourceInterval.dimension(0),//
 										sourceInterval.dimension(1),
 										Math.min(sourceInterval.dimension(2), cellDimensions[2])),
 								0,
@@ -310,39 +342,46 @@ public class DeformToAligned {
 				H5Utils.saveAttribute(resolution, writer, labelsPath, "resolution");
 			H5Utils.saveAttribute(new double[]{0, 0, 0}, writer, labelsPath, "offset");
 		}
-		
+
 		rawReader.close();
 		labelsReader.close();
 		writer.close();
-			
+
 		/* annotations */
 		if (transformResolution == null)
 			transformResolution = new double[]{1, 1, 1};
-		
+		final double[] annotationsOffset = offset == null ? new double[]{0, 0, 0} : offset;
+
 		final double[] annotationsResolution = transformResolution;
-		
+
 		final AnnotationsHdf5Store annotationsStore = new AnnotationsHdf5Store(params.inFileAnnotations, new LocalIdService());
 		final Annotations annotations = annotationsStore.read();
-		
+
 		final Collection<Annotation> annotationsCollection = annotations.getAnnotations();
 		annotationsCollection.forEach(
 				a -> {
 					final double[] slicePosition = new double[2];
 					final RealPoint position = a.getPosition();
-					slicePosition[0] = position.getDoublePosition(0) / annotationsResolution[2];
-					slicePosition[1] = position.getDoublePosition(1) / annotationsResolution[1];
-					final int zIndex = (int)Math.round(position.getDoublePosition(2) / annotationsResolution[0]);
+					slicePosition[0] = (position.getDoublePosition(0) + annotationsOffset[0]) / annotationsResolution[2];
+					slicePosition[1] = (position.getDoublePosition(1) + annotationsOffset[1]) / annotationsResolution[1];
+					final int zIndex = (int)Math.round((position.getDoublePosition(2) + annotationsOffset[2]) / annotationsResolution[0]);
+					System.out.println(zIndex);
+					System.out.print(net.imglib2.util.Util.printCoordinates(slicePosition) + " -> ");
 					if (zIndex >= 0 && zIndex < transforms.size()) {
 						final CoordinateTransform transform = transforms.get(zIndex);
-						transform.applyInPlace(slicePosition);	
+						transform.applyInPlace(slicePosition);
+						System.out.println(net.imglib2.util.Util.printCoordinates(slicePosition));
 					}
-					position.setPosition(slicePosition[0] * annotationsResolution[2], 0);
-					position.setPosition(slicePosition[1] * annotationsResolution[1], 1);
+					position.setPosition((slicePosition[0] * annotationsResolution[2]), 0);
+					position.setPosition((slicePosition[1] * annotationsResolution[1]), 1);
+					position.move(annotationsOffset[2], 2);
 				});
-		
+
 		final AnnotationsHdf5Store annotationsOutStore = new AnnotationsHdf5Store(params.outFile, new LocalIdService());
 		annotationsOutStore.write(annotations);
-		
+
+		System.out.println("Done");
+
 //			Util.display(
 //					RealViews.affine(
 //							Views.interpolate(
