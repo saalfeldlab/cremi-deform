eab822bcdeabde87324f05a0202bb40d33246313
Stephan Saalfeld <saalfelds@janelia.hhmi.org>
2019-02-14 19:28:10 -0500

    move dsbconvert

--- src/main/java/org/saalfeldlab/dsb/DSBConvert.java
+++ src/main/java/org/saalfeldlab/dsb/DSBConvert.java
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
-package org.janelia.saalfeldlab.deform;
+package org.saalfeldlab.dsb;
 
 import java.io.IOException;
 import java.nio.file.Files;
